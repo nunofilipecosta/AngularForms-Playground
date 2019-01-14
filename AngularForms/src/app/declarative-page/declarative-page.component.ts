ff4b8d706dc6a3e867c43f089838502fb88d5f64
Nuno Costa <nunofilipe.costa@gmail.com>
2019-01-14 21:53:43 +0000

    FIXED : declarative form

--- AngularForms/src/app/declarative-page/declarative-page.component.ts
+++ AngularForms/src/app/declarative-page/declarative-page.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeclarativePageComponent implements OnInit {
 
+  model = { firstName : '', lastName : '' , isFullTime : true, paymentType : 'W2', primaryLanguage: 'EN'};
+  languages = ['EN', 'FR'];
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  lastNameToUpper(value: string): string {
+    return value.toUpperCase();
   }
 
 }
